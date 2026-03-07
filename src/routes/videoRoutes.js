0b1ec8fc2150e7290f11940893b250e58a644342
adit-ya15 <aditya262701@gmail.com>
2026-03-07 23:08:28 +0530

    created uplaods folder

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -4,15 +4,22 @@ const videoRouter = express.Router();
 const videoController = require("../controllers/videoController")
 const { userAuth } = require("../middlewares/auth")
 const multer = require("multer");
+const fs = require("fs");
+
+const uploadDir = './uploads/';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/');
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
     }
 });
-const upload = multer({storage, limits: { fileSize: 100 * 1024 * 1024 }});
+const upload = multer({ storage, limits: { fileSize: 100 * 1024 * 1024 } });
 
 
 videoRouter.post("/upload", userAuth, upload.single("video"), videoController.uploadVideo);
